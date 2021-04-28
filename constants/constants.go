f5534fd188132730b1390ca74979513206cba746
xyz <11524882+xyzmap@users.noreply.github.com>
2021-04-29 01:46:11 +0200

    Add languages
    - Also throws an error if difficulty not supported

--- constants/constants.go
+++ constants/constants.go
@@ -1,8 +1,11 @@
 package constants
 
 const LOGGING_LEVEL string = "error"
+
 const AES_BLOCKSIZE_ERROR string = "AES BlockSize error: ciphertext too short"
 const AES_PLAINTEXT_NOT_MULTIPLE_ERROR string = "Plaintext is not a multiple of the block size"
+
+const BAD_REQUEST_DIFFICULTY_ERROR string = "Provided difficulty not supported"
 
 const GENERATE_MODE string = "generate"
 const DECRYPT_MODE string = "decrypt"
@@ -10,6 +13,23 @@ const ENCRYPT_MODE string = "encrypt"
 
 const RAW_OUTPUT string = "raw"
 const MNEMONIC_OUTPUT string = "mnemonic"
+
+const ENGLISH_LANGUAGE string = "english"
+const SPANISH_LANGUAGE string = "spanish"
+const CHINESE_TRADITIONAL_LANGUAGE string = "chinese_trad"
+const CHINESE_SIMPLIFIED_LANGUAGE string = "chinese_simp"
+const CZECH_LANGUAGE string = "czech"
+const FRENCH_LANGUAGE string = "french"
+const ITALIAN_LANGUAGE string = "italian"
+const JAPANESE_LANGUAGE string = "japanese"
+const KOREAN_LANGUAGE string = "korean"
+
+const MINIMUM_DIFFICULTY string = "minimum"
+const LOW_DIFFICULTY string = "low"
+const NORMAL_DIFFICULTY string = "normal"
+const STRONG_DIFFICULTY string = "strong"
+const SUPER_STRONG_DIFFICULTY string = "super_strong"
+const RIDICULOUSLY_STRONG_DIFFICULTY string = "ridiculously_strong"
 
 const (
 	UNKNOWN int = iota - 1
